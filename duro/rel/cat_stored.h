93be5849904b7393d93e1ed9b4d96c529780c464
rhartmann <none@none>
2015-07-20 19:33:15 +0200

    Removed arg names, fixed copyright year.

--- duro/rel/cat_stored.h
+++ duro/rel/cat_stored.h
@@ -1,7 +1,7 @@
 /*
  * Declarations of catalog functions dealing with physical tables and indexes.
  *
- * Copyright (C) 2005, 2012-2013 Rene Hartmann.
+ * Copyright (C) 2015 Rene Hartmann.
  * See the file COPYING for redistribution information.
  */
 
@@ -12,26 +12,26 @@
 #include "internal.h"
 
 int
-RDB_cat_insert_index(const char *name, int attrc, const RDB_seq_item attrv[],
-        RDB_bool unique, RDB_bool ordered, const char *tbname,
+RDB_cat_insert_index(const char *, int, const RDB_seq_item[],
+        RDB_bool, RDB_bool, const char *,
         RDB_exec_context *, RDB_transaction *);
 
 int
-RDB_cat_get_indexes(const char *tablename, RDB_dbroot *dbrootp,
+RDB_cat_get_indexes(const char *, RDB_dbroot *,
         RDB_exec_context *, RDB_transaction *, struct RDB_tbindex **);
 
 int
-RDB_cat_insert_table_recmap(RDB_object *tbp, const char *rmname,
-        RDB_exec_context *, RDB_transaction *txp);
+RDB_cat_insert_table_recmap(RDB_object *, const char *,
+        RDB_exec_context *, RDB_transaction *);
 
 int
 RDB_cat_recmap_name(RDB_object *, RDB_object *, RDB_exec_context *,
         RDB_transaction *);
 
 int
-RDB_string_to_id(RDB_object *id, const char *str, RDB_exec_context *ecp);
+RDB_string_to_id(RDB_object *, const char *, RDB_exec_context *);
 
 RDB_expression *
-RDB_tablename_id_eq_expr(const char *name, RDB_exec_context *ecp);
+RDB_tablename_id_eq_expr(const char *, RDB_exec_context *);
 
 #endif /* CAT_STORED_H_ */
