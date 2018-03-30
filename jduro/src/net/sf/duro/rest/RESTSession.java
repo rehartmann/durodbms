bdb0a2780d3f266dfb3e281cceb95001edb9dc4e
rhartmann@users.sourceforge.net <rhartmann@users.sourceforge.net>
2018-03-30 10:24:57 +0200

    Code cleanup

--- jduro/src/net/sf/duro/rest/RESTSession.java
+++ jduro/src/net/sf/duro/rest/RESTSession.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
