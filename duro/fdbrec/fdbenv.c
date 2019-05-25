109b1cf5c50d4fc1331fd1aa03b3d1aed24dfd37
rhartmann@users.sourceforge.net <rhartmann@users.sourceforge.net>
2019-05-25 13:35:15 +0200

    Using pthreads for FoundationDB under Linux

--- duro/fdbrec/fdbenv.c
+++ duro/fdbrec/fdbenv.c
@@ -5,24 +5,35 @@
 #include <fdbrec/fdbsequence.h>
 #include <fdbrec/fdbtx.h>
 
+#ifdef _WIN32
 #include <windows.h>
 #include <strsafe.h>
+#else
+#include <pthread.h>
+#endif
 
 #include <stdio.h>
 
 static RDB_bool RDB_fdb_initialized = RDB_FALSE;
 
-DWORD WINAPI
-RDB_event_loop(_In_ LPVOID lpParameter)
+void *
+RDB_event_loop(void *arg)
 {
-	int err;
-
-	err = fdb_run_network();
-	if (err != 0) {
-		printf("error %d\n", err);
-	}
-	return (DWORD) 0;
+    int err = fdb_run_network();
+    if (err != 0) {
+        printf("error %d\n", err);
+    }
+    return NULL;
 }
+
+#ifdef _WIN32
+DWORD WINAPI
+RDB_win_event_loop(_In_ LPVOID lpParameter)
+{
+    RDB_event_loop(NULL);
+    return (DWORD) 0;
+}
+#endif
 
 static int
 RDB_fdb_close_env(RDB_environment *envp, RDB_exec_context *ecp)
@@ -49,85 +60,92 @@ fdb_get_errfile(const RDB_environment *envp)
 RDB_environment *
 RDB_fdb_open_env(const char *path, RDB_exec_context *ecp)
 {
-	fdb_error_t err;
-	FDBCluster *cluster;
-	FDBFuture *f;
-	RDB_environment *envp;
+    fdb_error_t err;
+    FDBCluster *cluster;
+    FDBFuture *f;
+    RDB_environment *envp;
 
-	if (!RDB_fdb_initialized) {
-		err = fdb_select_api_version(600);
-		if (err != 0) {
+    if (!RDB_fdb_initialized) {
+#ifndef _WIN32
+        pthread_t thread;
+#endif
+
+        err = fdb_select_api_version(600);
+        if (err != 0) {
             RDB_handle_fdb_errcode(err, ecp, NULL);
-			return (DWORD) 0;
-		}
-		RDB_fdb_initialized = RDB_TRUE;
+            return NULL;
+        }
+        RDB_fdb_initialized = RDB_TRUE;
 
-		err = fdb_setup_network();
-		if (err != 0) {
+        err = fdb_setup_network();
+        if (err != 0) {
             RDB_handle_fdb_errcode(err, ecp, NULL);
-			return NULL;
-		}
+            return NULL;
+        }
+#ifdef _WIN32
+        if (CreateThread(NULL, 0, &RDB_win_event_loop, NULL, 0, NULL) == NULL) {
+#else
+        if (pthread_create(&thread, NULL, &RDB_event_loop, NULL) != 0) {
+#endif
+            RDB_raise_system("thread creation failed", ecp);
+            return NULL;
+        }
+    }
 
-		if (CreateThread(NULL, 0, &RDB_event_loop, NULL, 0, NULL) == NULL) {
-			RDB_raise_system("CreateThread() failed", ecp);
-			return NULL;
-		}
-	}
+    envp = malloc(sizeof(RDB_environment));
+    if (envp == NULL) {
+        RDB_raise_no_memory(ecp);
+        return NULL;
+    }
+    envp->close_fn = &RDB_fdb_close_env;
+    envp->create_recmap_fn = &RDB_create_fdb_recmap;
+    envp->open_recmap_fn = &RDB_open_fdb_recmap;
+    envp->open_sequence_fn = &RDB_open_fdb_sequence;
+    envp->rename_sequence_fn = &RDB_rename_fdb_sequence;
+    envp->begin_tx_fn = &RDB_fdb_begin_tx;
+    envp->commit_fn = &RDB_fdb_commit;
+    envp->abort_fn = &RDB_fdb_abort;
+    envp->tx_id_fn = &RDB_fdb_tx_id;
+    envp->set_errfile_fn = &fdb_set_errfile;
+    envp->get_errfile_fn = &fdb_get_errfile;
 
-	envp = malloc(sizeof(RDB_environment));
-	if (envp == NULL) {
-		RDB_raise_no_memory(ecp);
-		return NULL;
-	}
-	envp->close_fn = &RDB_fdb_close_env;
-	envp->create_recmap_fn = &RDB_create_fdb_recmap;
-	envp->open_recmap_fn = &RDB_open_fdb_recmap;
-	envp->open_sequence_fn = &RDB_open_fdb_sequence;
-	envp->rename_sequence_fn = &RDB_rename_fdb_sequence;
-	envp->begin_tx_fn = &RDB_fdb_begin_tx;
-	envp->commit_fn = &RDB_fdb_commit;
-	envp->abort_fn = &RDB_fdb_abort;
-	envp->tx_id_fn = &RDB_fdb_tx_id;
-	envp->set_errfile_fn = &fdb_set_errfile;
-	envp->get_errfile_fn = &fdb_get_errfile;
+    envp->cleanup_fn = NULL;
+    envp->xdata = NULL;
+    envp->trace = 0;
+    envp->queries = RDB_FALSE;
 
-	envp->cleanup_fn = NULL;
-	envp->xdata = NULL;
-	envp->trace = 0;
-	envp->queries = RDB_FALSE;
-
-	f = fdb_create_cluster(path);
-	err = fdb_future_block_until_ready(f);
-	if (err != 0) {
-		fdb_future_destroy(f);
+    f = fdb_create_cluster(path);
+    err = fdb_future_block_until_ready(f);
+    if (err != 0) {
+        fdb_future_destroy(f);
         RDB_handle_fdb_errcode(err, ecp, NULL);
-		free(envp);
-		return NULL;
-	}
-	if ((err = fdb_future_get_cluster(f, &cluster)) != 0) {
-		fdb_future_destroy(f);
+        free(envp);
+        return NULL;
+    }
+    if ((err = fdb_future_get_cluster(f, &cluster)) != 0) {
+        fdb_future_destroy(f);
         RDB_handle_fdb_errcode(err, ecp, NULL);
-		free(envp);
-		return NULL;
-	}
-	fdb_future_destroy(f);
-	f = fdb_cluster_create_database(cluster, (uint8_t const*) "DB", 2);
-	err = fdb_future_block_until_ready(f);
-	if (err != 0) {
-		fdb_future_destroy(f);
+        free(envp);
+        return NULL;
+    }
+    fdb_future_destroy(f);
+    f = fdb_cluster_create_database(cluster, (uint8_t const*) "DB", 2);
+    err = fdb_future_block_until_ready(f);
+    if (err != 0) {
+        fdb_future_destroy(f);
         RDB_handle_fdb_errcode(err, ecp, NULL);
-		free(envp);
-		return NULL;
-	}
-	if ((err = fdb_future_get_database(f, &envp->env.fdb)) != 0) {
-		fdb_future_destroy(f);
+        free(envp);
+        return NULL;
+    }
+    if ((err = fdb_future_get_database(f, &envp->env.fdb)) != 0) {
+        fdb_future_destroy(f);
         RDB_handle_fdb_errcode(err, ecp, NULL);
-		free(envp);
-		return NULL;
-	}
-	fdb_future_destroy(f);
+        free(envp);
+        return NULL;
+    }
+    fdb_future_destroy(f);
 
-	return envp;
+    return envp;
 }
 
 void
